5e00dd797ba63ee1b95f7ac3b045e9cfad325ab8
louis-pham <l.c.pham@hotmail.com>
2020-02-14 15:41:40 -0500

    Updates

---
+++ precache-manifest.7f64e4358d0f345e9e15d11d8a078db0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de576f118fe410054411bda18bd5e041",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "30ba3830eeefc150c9ee",
+    "url": "/project-4-assessment/static/css/main.d65befc5.chunk.css"
+  },
+  {
+    "revision": "50791072ca96be2954d2",
+    "url": "/project-4-assessment/static/js/2.4a494911.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/project-4-assessment/static/js/2.4a494911.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30ba3830eeefc150c9ee",
+    "url": "/project-4-assessment/static/js/main.a38348a3.chunk.js"
+  },
+  {
+    "revision": "9923933f7998bf1ba10e",
+    "url": "/project-4-assessment/static/js/runtime-main.6f46026f.js"
+  }
+]);
